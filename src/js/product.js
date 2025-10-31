e5ab652c66076f49b37ed70395e955cc5e47ddda
Argie <argiedc888@gmail.com>
2025-10-31 18:08:45 +0800

    Done I guess?  I did nothing but its working now

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,9 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData("tents");
 
+// I did nothing but its working now
+const dataSource = new ProductData("tents");
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
   cartItems.push(product);
