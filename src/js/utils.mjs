cea900208e648eea060998c79d6453a030b2c237
WizardAaron <ethanlong547@gmail.com>
2025-11-09 19:26:31 -0800

    updated utils with getParam

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,6 +2,14 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const product = urlParams.get('product');
 
+// small Update, more soon
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product
+}
+
 // wrapper for querySelector...returns matching element
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
